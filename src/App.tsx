af9d77e5ef2f7e4d111bce8a94c9da2eeae5d146
Try Ajitiono <ballinst@gmail.com>
2019-10-04 20:52:29 +1000

    Update basePath
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import RootRoutes from './router';
 
 function App() {
-  return <RootRoutes />;
+  return (
+    <Router>
+      <RootRoutes />
+    </Router>
+  );
 }
 
 export default App;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, useHistory } from 'react-router-dom';
 import EuiCustomLink from './EuiCustomLink';
 import Home from '../views/Home';
 import About from '../views/About';
 
-export default function RootRoutes() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul className="nav">
-            <li className="list-item">
-              <EuiCustomLink to="/">Home</EuiCustomLink>
-            </li>
-            <li className="list-item">
-              <EuiCustomLink to="/about">About</EuiCustomLink>
-            </li>
-          </ul>
+const basePath = '/elastic-react-router-hooks';
+const homePath = `${basePath}/`;
+const aboutPath = `${basePath}/about`;
 
-          <hr />
-        </nav>
-        <main className="content">
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </main>
-      </div>
-    </Router>
+export default function RootRoutes() {
+  const history = useHistory();
+
+  useEffect(() => {
+    history.push(basePath);
+  }, [history]);
+
+  return (
+    <div>
+      <nav>
+        <ul className="nav">
+          <li className="list-item">
+            <EuiCustomLink to={homePath}>Home</EuiCustomLink>
+          </li>
+          <li className="list-item">
+            <EuiCustomLink to={aboutPath}>About</EuiCustomLink>
+          </li>
+        </ul>
+
+        <hr />
+      </nav>
+      <main className="content">
+        <Route exact path={homePath} component={Home} />
+        <Route path={aboutPath} component={About} />
+      </main>
+    </div>
   );
 }
